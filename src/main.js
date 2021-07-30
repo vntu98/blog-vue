ec8e15aa11b6f4b4cdd5674bbf8f8c925f8faa60
vntu <vntu98@gmail.com>
2021-07-30 19:29:47 +0700

    show post detail

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/app.css'
 import router from './router'
+import axios from 'axios'
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.withCredentials = true
+
+createApp(App).use(router).use(store).mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,36 @@
 <template>
-    <div>
-        Home
+    <div class="space-y-16">
+        <div v-for="post in posts" :key="post.uuid">
+            <router-link :to="{ name: 'post', params: { slug: post.slug } }" class="block">
+                <h1 class="text-3xl sm:text-4xl leading-10 font-extrabold tracking-tight text-gray-900">
+                    {{ post.title }}
+                </h1>
+                <p class="mt-6 text-gray-500">
+                    {{ post.teaser }}
+                </p>
+            </router-link>
+        </div>
     </div>
 </template>
+
+<script>
+import { onMounted } from 'vue'
+import usePosts from '../api/usePosts'
+
+import { useStore } from 'vuex'
+
+export default {
+    setup() {
+        const { posts, fetchPosts } = usePosts()
+
+        const store = useStore()
+        console.log(store.getters.authenticated)
+
+        onMounted(fetchPosts)
+
+        return {
+            posts
+        }
+    },
+}
+</script>
